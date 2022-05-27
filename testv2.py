869c9758f0cbac79a30d87d62f9900d73cb1acda
qod3r <primculov@protonmail.ch>
2022-05-27 13:17:20 +0700

    added top plays

--- testv2.py
+++ testv2.py
@@ -26,12 +26,24 @@ def user_v2(api, uid):
     return res
 
 
-def calc_acc(_300, _100, _50, _miss):
-    acc = (300*_300 + 100*_100 + 50*_50)/(300*(_300 + _100 + _50 + _miss)) * 100
-    return acc
+def calc_acc(score):
+    _300 = score.count_300
+    _100 = score.count_100
+    _50 = score.count_50
+    _miss = score.count_miss
+    return (300*_300 + 100*_100 + 50*_50)/(300*(_300 + _100 + _50 + _miss)) * 100
 
-def recent(apiv1, apiv2, uid):
-    score = apiv1.get_user_recent(uid)[0]
+def calc_completion(score, b):
+    return (score.count_300 + score.count_100 + score.count_50 + score.count_miss)/(b.count_hitcircles+b.count_sliders+b.count_spinners)*100
+
+def recent(apiv1, apiv2, uid, index=None):
+    if index is not None:
+        if index > 50 or index == 0:
+            return "Доступны только последние 50 скоров"
+    else:
+        index = 1
+        
+    score = apiv1.get_user_recent(uid, 0, index)[index-1]
     b = apiv1.get_beatmaps(beatmap_id=score.beatmap_id)[0]
     map_status = {4: "Loved", 3: "Qualified", 2: "Approved", 1: "Ranked", 0: "Pending", -1: "WIP", -2: "Graveyard"}
     
@@ -40,17 +52,47 @@ def recent(apiv1, apiv2, uid):
     {b.total_length//60:.0f}:{b.total_length%60:.0f} | AR:{b.approach_rate} CS:{b.circle_size} OD:{b.overrall_difficulty} HP:{b.health} {b.bpm}BPM | {b.star_rating:.2f}✩ {f"+{score.mods.short_name()}" if score.mods.value != 0 else ""}
     
     Score: {score.score} | Combo: {score.max_combo}x/{b.max_combo}x
-    Accuracy: {calc_acc(score.count_300, score.count_100, score.count_50, score.count_miss):.2f}%
+    Accuracy: {calc_acc(score):.2f}%
     PP: {0 if score.pp is None else score.pp}
     Hitcounts: {score.count_300}/{score.count_100}/{score.count_50}/{score.count_miss}
-    Grade: {score.rank} ({(score.count_300 + score.count_100 + score.count_50 + score.count_miss)/(b.count_hitcircles+b.count_sliders+b.count_spinners)*100:.2f}%)
+    Grade: {score.rank} ({calc_completion(score, b):.2f}%)
     
     Beatmap: https://osu.ppy.sh/b/{b.beatmap_id}
     '''
 
 
+def top_plays(apiv1, uid, index=None):
+    if index is not None:
+        if index > 100 or index == 0:
+            return "Доступны только последние 100 скоров"
+        else:
+            scores = apiv1.get_user_best(uid, 0, index)[index-1:index]
+    else:
+        scores = apiv1.get_user_best(uid, 0, index)[:3]
+        index = 1
+    
+    player_name = apiv1.get_user(uid).username
+    res = f"Топ скоры игрока {player_name}:"
+    for score in scores:
+        b = apiv1.get_beatmaps(beatmap_id=score.beatmap_id)[0]
+        res += f'''
+        #{index}
+        {b.title} [{b.version}] {f"+{score.mods.short_name()}" if score.mods.value != 0 else ""}
+        AR:{b.approach_rate} CS:{b.circle_size} OD:{b.overrall_difficulty} HP:{b.health} {b.star_rating:.2f}✩
+        Grade: {score.rank} ⯈ {score.max_combo}x/{b.max_combo}x ⯈ {b.total_length//60:.0f}:{b.total_length%60:.0f}
+        Accuracy: {calc_acc(score):.2f}% ⯈ {score.count_300}/{score.count_100}/{score.count_50}/{score.count_miss}
+        PP: {score.pp}
+        {score.date}
+        https://osu.ppy.sh/b/{b.beatmap_id}
+        '''
+        index += 1
+    return res
+
+
 apiv2 = OssapiV2(CLIENT_ID, CLIENT_SECRET)
 apiv1 = Ossapi(API_KEY)
 
-print(user_v2(apiv2, "qod3r"))
-print(recent(apiv1, apiv2, "qod3r"))
+
+# print(user_v2(apiv2, "qod3r"))
+# print(recent(apiv1, apiv2, "whitecat"))
+print(top_plays(apiv1, "qod3r"))
